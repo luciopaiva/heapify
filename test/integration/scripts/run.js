b91098939877eb62f53fd993957dae3f161f0fc8
Lucio Paiva <luciopaiva@gmail.com>
2022-01-05 15:16:33 +0000

    Move integration test scripts to own folder

--- test/integration/scripts/run.js
+++ test/integration/scripts/run.js
@@ -1,7 +1,8 @@
 
 const spawn = require("child_process").spawn;
 
-const tests = ["node-import", "node-require", "node-typescript"];
+const TESTS = ["node-import", "node-require", "node-typescript"];
+const SCRIPTS_FOLDER = "scripts";
 
 async function exec(cmd, args) {
     const process = spawn(cmd, args, { stdio: "inherit" });
@@ -10,7 +11,7 @@ async function exec(cmd, args) {
 
 async function runTest(folder) {
     try {
-        await exec(`./run-integration-test.sh`, [folder]);
+        await exec(`${SCRIPTS_FOLDER}/run-integration-test.sh`, [folder]);
     } catch (e) {
         console.error(e.message.trim());
         throw e;
@@ -18,9 +19,9 @@ async function runTest(folder) {
 }
 
 async function run() {
-    await exec("./setup-integration-tests.sh");
+    await exec(`${SCRIPTS_FOLDER}/setup-integration-tests.sh`);
 
-    const results = await Promise.allSettled(tests.map(runTest));
+    const results = await Promise.allSettled(TESTS.map(runTest));
     if (results.some(result => result.status === "rejected")) {
         console.error("There are failing integration tests.");
     } else {
