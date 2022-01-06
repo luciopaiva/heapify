90d136ac08205a32a280ddbeaa2676ea47c91ef8
Lucio Paiva <luciopaiva@gmail.com>
2022-01-06 21:24:00 +0000

    Port Webpack config to TypeScript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+import * as path from "path";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 
 let isFirstTarget = true;
 
-function makeConfig(mode, target, filename, module) {
+function makeConfig(mode: string, target: string, filename: string, module: boolean) {
     const config = {
         mode: mode,
         entry: "./src/heapify.ts",
@@ -36,9 +36,9 @@ function makeConfig(mode, target, filename, module) {
         },
         plugins: [
             new ForkTsCheckerWebpackPlugin({
-                // we want to generate d.ts files, but only for the first target (no need to generate them 3 times!)
+                // we want to generate a d.ts file, but only for the first target (no need to generate it 3 times!)
                 ...isFirstTarget && { typescript: {
-                    build: true,  // see https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/689#issuecomment-1005020849
+                    // build: true,  // see https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/689#issuecomment-1005020849
                     mode: "write-dts",
                 }},
             }),
@@ -52,7 +52,7 @@ function makeConfig(mode, target, filename, module) {
     return config;
 }
 
-module.exports = (mode) => [
+export default (mode: string) => [
     makeConfig(mode, "node", "heapify.node.js", false),
     makeConfig(mode, "web", "heapify.js", false),
     makeConfig(mode, "web", "heapify.mjs", true),
