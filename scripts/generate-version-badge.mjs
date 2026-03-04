24c223dcb217c41739aa1c113bb12419371be623
Lucio Paiva <luciopaiva@gmail.com>
2026-03-04 14:31:46 +0000

    Update version badge

--- scripts/generate-version-badge.mjs
+++ scripts/generate-version-badge.mjs
@@ -7,7 +7,7 @@ const root = resolveRoot(import.meta.url);
 // Read version from package.json
 const { version } = JSON.parse(readFileSync(join(root, "package.json"), "utf8"));
 
-const svg = generateBadgeSvg("version", `v${version}`, "#4c1");
+const svg = generateBadgeSvg("version", `v${version}`, "#007ec6");
 writeBadge(root, "version.svg", svg);
 
 // eslint-disable-next-line no-console
